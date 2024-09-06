b989f17c6634c0cf73a2e2cf60d015560f97b78a
mehmetdayioglu <49623562+mehmetdayioglu@users.noreply.github.com>
2024-09-06 16:08:08 +0300

    Create KeypadKeyboard.ino
    # Arduino Pro Micro 4x4 Keypad Matrix Keyboard Emulation
    
    This project is designed for use with an Arduino Pro Micro and a 4x4 keypad matrix. The code enables the Arduino to read input from the keypad and emulate keyboard input, sending keystrokes to a connected computer based on the pressed keys.

---
+++ KeypadKeyboard.ino
@@ -0,0 +1,34 @@
+#include <Keypad.h>
+#include "Keyboard.h"
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+byte rowPins[ROWS] = { 2, 3, 4, 5 };
+byte colPins[COLS] = { 6, 7, 8, 9 };
+
+char customKeymap[ROWS][COLS] = {
+  { '1', '2', '3', '4' },
+  { 'a', 'b', 'c', 'd' },
+  { 'k', 'l', 'm', 'n' },
+  { 'u', 'v', 'w', 'x' }
+};
+
+Keypad keypad = Keypad(makeKeymap(customKeymap), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+  Keyboard.begin();
+}
+
+void loop() {
+
+  char key = keypad.getKey();
+  int state = keypad.getState();
+
+  if ( state!=0 ) { 
+    Keyboard.press(key);} 
+    
+  else { 
+    Keyboard.releaseAll();}
+}
